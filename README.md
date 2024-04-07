95d5de9fa5e5fe7ab8aeaa18fe1e08eeed23b990
Sukhdev Patil <sukhadev.patil@gmail.com>
2024-04-07 11:46:56 +0530

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-Basics for the java developers to get hand dirty on MongoDB
+Basics for the java developers to get hand dirty on MongoDB.
+[Note - Use "books.json" file to load sample data]
 
-1. MongodbJavaPlatformApplicationTests.testConnection() :: MongoDB Connection configuration - MongoDbConnection
-2. 
+1. MongodbJavaPlatformApplicationTests.testConnection() :: Check MongoDB Connection
+2. MongodbJavaPlatformApplicationTests.testDatabases() :: Get Databases list
+3. MongodbJavaPlatformApplicationTests.insertOneRecord() :: Insert one record
+4. MongodbJavaPlatformApplicationTests.insertMultipleRecords() :: Insert multiple records
+5. MongodbJavaPlatformApplicationTests.testQueryingCollection() :: Find all records for matching conditions
+6. MongodbJavaPlatformApplicationTests.testQueryingFindFirstCollection() :: Find first record for matching condition(s)
+7. MongodbJavaPlatformApplicationTests.updateOneRecord() :: Updates one record
+8. MongodbJavaPlatformApplicationTests.updateManyRecord() :: Updates many records
+9. MongodbJavaPlatformApplicationTests.deleteOneRecord() :: Delete one record
+10. MongodbJavaPlatformApplicationTests.deleteManyRecord() :: Delete many records
+
--- src/test/java/com/mongodb/javaplatform/mongodbjavaplatform/MongodbJavaPlatformApplicationTests.java
+++ src/test/java/com/mongodb/javaplatform/mongodbjavaplatform/MongodbJavaPlatformApplicationTests.java
@@ -1,28 +1,216 @@
 package com.mongodb.javaplatform.mongodbjavaplatform;
 
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.InsertManyResult;
+import com.mongodb.client.result.InsertOneResult;
+import com.mongodb.client.result.UpdateResult;
+import org.bson.BsonValue;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.*;
+
+import static com.mongodb.client.model.Filters.*;
 
 @SpringBootTest
 class MongodbJavaPlatformApplicationTests {
 
-	@Test
-	//@Disabled
-	void contextLoads() {
-	}
+    @Test
+        //@Disabled
+    void contextLoads() {
+    }
+
+    @Test
+    void testConnection() {
+        MongoClient client = MongoDbConnection.getInstance();
+
+        System.out.println("==============");
+        client.listDatabaseNames().forEach(System.out::println);
+        System.out.println("=============");
+
+        Assertions.assertNotNull(client.listDatabaseNames());
+    }
+
+    @Test
+    void testDatabases() {
+        MongoClient client = MongoDbConnection.getInstance();
+        List<Document> databases = client.listDatabases().into(new ArrayList<>());
+
+        System.out.println("==============");
+        databases.forEach(db -> System.out.println(db.toJson()));
+        System.out.println("==============");
+
+        Assertions.assertNotNull(databases);
+    }
+
+    @Test
+    void insertOneRecord() {
+        MongoClient client = MongoDbConnection.getInstance();
+
+        MongoDatabase database = client.getDatabase("training");
+        MongoCollection<Document> collection = database.getCollection("inspections");
+
+        Document document = new Document("_id", new ObjectId())
+                .append("id", "10021-2015-ENFO")
+                .append("certificate_number", 9278806)
+                .append("business_name", "ATLIXCO DELI GROCERY INC.")
+                .append("date",
+						Date.from(LocalDate.of(2015, 2, 20).atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .append("result", "No Violation Issued")
+                .append("sector", "Cigarette Retail Dealer - 127")
+                .append("address",
+                        new Document()
+                                .append("city", "RIDGEWOOD")
+                                .append("zip", 11385)
+                                .append("street", "MENAHAN ST")
+                                .append("number", 1712));
+
+        InsertOneResult result = collection.insertOne(document);
+        BsonValue id = result.getInsertedId();
+        System.out.println(id);
+
+		Assertions.assertNotNull(id);
+    }
 
 	@Test
-	void testConnection() {
+	void insertMultipleRecords() {
 		MongoClient client = MongoDbConnection.getInstance();
 
-		System.out.println("==============");
-		client.listDatabaseNames().forEach(System.out::println);
-		System.out.println("=============");
+		MongoDatabase database = client.getDatabase("bank");
+		MongoCollection<Document> collection = database.getCollection("accounts");
 
-		Assertions.assertNotNull(client.listDatabaseNames());
+		Document doc1 = new Document().append("account_holder","john doe").append("account_id","MDB99115881").append("balance",1785).append("account_type","checking");
+		Document doc2 = new Document().append("account_holder","jane doe").append("account_id","MDB79101843").append("balance",1468).append("account_type","checking");
+		List<Document> accounts = Arrays.asList(doc1, doc2);
+
+		InsertManyResult result = collection.insertMany(accounts);
+		Map<Integer, BsonValue> mapIds = result.getInsertedIds();
+
+		System.out.println("==================");
+		mapIds.forEach((k, v) -> {
+			System.out.println(k + " :: val :: " + v);
+		});
+		System.out.println("==================");
+
+		Assertions.assertNotNull(mapIds);
 	}
+
+	@Test
+	void testQueryingCollection() {
+		MongoDatabase database = MongoDbConnection.getInstance().getDatabase("sample_data");
+
+		MongoCollection<Document> collection = database.getCollection("books");
+
+		List<Document> documents = collection.find(
+                and(eq("status", "PUBLISH"), gte("pageCount", 400), in("categories", "Java")))
+                .into(new ArrayList<>());
+
+		documents.forEach(doc -> System.out.println(doc.toJson()));
+
+		Assertions.assertNotNull(documents);
+	}
+
+    @Test
+    void testQueryingFindFirstCollection() {
+        MongoDatabase database = MongoDbConnection.getInstance().getDatabase("sample_data");
+
+        MongoCollection<Document> collection = database.getCollection("books");
+
+        Document document = collection.find(
+                        and(eq("status", "PUBLISH"),
+                                gte("pageCount", 400),
+                                in("categories", "Java")))
+                .first();
+
+        assert document != null;
+        System.out.println(document.toJson());
+
+        Assertions.assertNotNull(document.toJson());
+    }
+
+    @Test
+    void updateOneRecord() {
+        MongoDatabase database = MongoDbConnection.getInstance().getDatabase("sample_data");
+
+        MongoCollection<Document> collection = database.getCollection("books");
+
+        Bson query  = Filters.eq("isbn","1933988673");
+
+        Bson updates  = Updates.combine(Updates.set("account_status","active"),Updates.inc("pageCount",1));
+        UpdateResult upResult = collection.updateOne(query, updates);
+
+        System.out.println("===========");
+        System.out.println(upResult);
+        System.out.println("===========");
+
+        Assertions.assertNotNull(upResult);
+    }
+
+
+    @Test
+    void updateManyRecord() {
+        MongoDatabase database = MongoDbConnection.getInstance().getDatabase("sample_data");
+
+        MongoCollection<Document> collection = database.getCollection("books");
+
+        Bson query  = Filters.eq("status","PUBLISH");
+        Bson updates  = Updates.combine(Updates.set("account_status","active"),Updates.inc("pageCount",1));
+        UpdateResult upResult = collection.updateMany(query, updates);
+
+        System.out.println("===========");
+        System.out.println(upResult);
+        System.out.println("===========");
+
+        Assertions.assertNotNull(upResult);
+    }
+
+    @Test
+    void deleteOneRecord() {
+        MongoDatabase database = MongoDbConnection.getInstance().getDatabase("sample_data");
+
+        MongoCollection<Document> collection = database.getCollection("books");
+
+        Bson query  = Filters.lt("pageCount",100);
+
+        DeleteResult deleteResult = collection.deleteOne(query);
+
+        System.out.println("===========");
+        System.out.println(deleteResult);
+        System.out.println("===========");
+        System.out.println(deleteResult.getDeletedCount());
+        System.out.println("===========");
+
+        Assertions.assertNotNull(deleteResult);
+    }
+
+    @Test
+    void deleteManyRecord() {
+        MongoDatabase database = MongoDbConnection.getInstance().getDatabase("sample_data");
+
+        MongoCollection<Document> collection = database.getCollection("books");
+
+        Bson query  = Filters.lt("pageCount",100);
+
+        DeleteResult deleteResult = collection.deleteMany(query);
+
+        System.out.println("===========");
+        System.out.println(deleteResult);
+        System.out.println("===========");
+        System.out.println(deleteResult.getDeletedCount());
+        System.out.println("===========");
+
+        Assertions.assertNotNull(deleteResult);
+    }
 
 }
